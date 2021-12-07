a5edd97381e4b3c2c74ec4d6ee3ed60ec016abcb
rakis10 <jurajrak14@gmail.com>
2021-12-07 22:02:08 +0100

    evaluate

--- src/main/java/com/example/demo/TransactionService.java
+++ src/main/java/com/example/demo/TransactionService.java
@@ -2,6 +2,10 @@ package com.example.demo;
 
 import com.example.demo.models.Transaction;
 import com.example.demo.repos.TransactionRepository;
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -18,8 +22,24 @@ public class TransactionService {
 
     public String evaluate(Transaction transaction){
         transaction.setDate(new Date());
+        List<Transaction> transactions = transactionRepository.findPositionalParameter(transaction.getLastIP(), Sort.by(Sort.Direction.DESC, "date"));
         transactionRepository.save(transaction);
-        return "schvalene";
+        int i = 0;
+        int count_same_action = 0;
+        while (transactions.get(i) != null && i < 5){
+            if(transactions.get(i).getLastTransaction().equals(transaction.getLastTransaction())){
+              count_same_action++;
+            }
+            i++;
+        }
+
+        if (count_same_action<1){
+            return "high risk";
+        }else if (count_same_action<3){
+            return "medium risk";
+        }else {
+            return "low risk";
+        }
     }
 
     public List<Transaction> findAll() {
--- src/main/java/com/example/demo/repos/TransactionRepository.java
+++ src/main/java/com/example/demo/repos/TransactionRepository.java
@@ -1,7 +1,15 @@
 package com.example.demo.repos;
 
 import com.example.demo.models.Transaction;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.List;
 
 public interface TransactionRepository extends MongoRepository<Transaction, String> {
+//    List<Transaction> find(Query query, Class<Transaction> transactionClass);
+
+    @Query("{'lastIP' : ?0}")
+    List<Transaction> findPositionalParameter(String lastIP, Sort date);
 }
