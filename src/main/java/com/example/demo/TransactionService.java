3c8f55d35681d04cb2ac1a11b01dbfea90445319
rakis10 <jurajrak14@gmail.com>
2022-01-03 16:55:09 +0100

    fix save mongo

--- src/main/java/com/example/demo/TransactionService.java
+++ src/main/java/com/example/demo/TransactionService.java
@@ -20,16 +20,16 @@ public class TransactionService {
 
     public ResponseEntity<?> evaluate(Transaction transaction){
         int count_same_action = 0;
-//        transaction.setDate(new Date());
-//        List<Transaction> transactions = transactionRepository.findPositionalParameter(transaction.getLastIP(), Sort.by(Sort.Direction.DESC, "date"));
-//        transactionRepository.save(transaction);
-//        int i = 0;
-//        while (transactions.get(i) != null && i < 5){
-//            if(transactions.get(i).getLastTransaction().equals(transaction.getLastTransaction())){
-//              count_same_action++;
-//            }
-//            i++;
-//        }
+        transaction.setDate(new Date());
+        List<Transaction> transactions = transactionRepository.findPositionalParameter(transaction.getLastIP(), Sort.by(Sort.Direction.DESC, "date"));
+        transactionRepository.save(transaction);
+        int i = 0;
+        while (transactions.get(i) != null && i < 5){
+            if(transactions.get(i).getLastTransaction().equals(transaction.getLastTransaction())){
+              count_same_action++;
+            }
+            i++;
+        }
 
         if (count_same_action<1){
             return new ResponseEntity<>("high risk", HttpStatus.OK);
